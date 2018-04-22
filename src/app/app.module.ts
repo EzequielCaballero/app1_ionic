b3530b2c1ec07a652d03c31bfba1255d4ef36f45
EzequielCaballero <ezequiel.caballero33@gmail.com>
2018-04-21 21:47:01 -0300

    Validación interna de usuarios a partir de json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { FormsModule } from '@angular/forms'
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -16,7 +17,8 @@ import { LoginPage } from '../pages/login/login';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    FormsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+//MANEJO DE DATOS
+import { USUARIOS } from "../../data/data_usuarios"; // FUENTE
+import { Usuario } from "../../interfaces/usuario_interface"; //FORMATO
 
 @Component({
   selector: 'page-login',
@@ -7,11 +12,35 @@ import { NavController} from 'ionic-angular';
 })
 export class LoginPage {
 
+  //ATRIBUTOS
+  usuarios:Usuario[] = [];
+  userNameInput:String = "";
+  userPassInput:Number;
+
+  //CONSTRUCTOR
   constructor(public navCtrl: NavController) {
+    this.usuarios = USUARIOS.slice(0);
   }
 
+  //METODOS
   validarUsuario(){
     console.log("Validando usuario...");
+    console.info(this.usuarios);
+    for(let user of this.usuarios){
+      if(this.userNameInput == user.nombre && this.userPassInput == user.clave)
+      {
+        this.ingresar();
+        break;
+      }
+      else
+        console.log("Error al iniciar sesión!");
+    }
+    // this.usuarios.forEach(function(){
+    // });
+  }
+
+  ingresar(){
+    this.navCtrl.push(HomePage);
   }
 
 }
